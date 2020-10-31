45b48c7adb58db8393feb1d65a5f64e212e3eb3b
Mauricio221911210 <al221911210@gmail.com>
2020-10-30 22:53:08 -0600

    Mostrar una lista de elemntos

---
+++ Desktop/Proyectos APPS/componentes/lib/src/pages/home_temp.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+import 'package:componentes/src/pages/home_temp.dart';
+
+
+class HomePageTemp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Home Temp'),
+    );
+  }
+}
